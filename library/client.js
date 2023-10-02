5bbe4a4d57a1c75b08027092823d5824ade886a5
HimashaNethmini <himashanethmini05@gmail.com>
2023-10-02 21:58:48 +0530

    completed setting up sanity for client

--- library/client.js
+++ library/client.js
@@ -1,4 +1,5 @@
 import sanityClient from "@sanity/client";
+import { ImageUrlBuilder } from "@sanity/image-url/lib/types/builder";
 
 export const client = sanityClient({
   projectId: "h7hytqn1",
@@ -8,3 +9,7 @@ export const client = sanityClient({
   token:
     "skcEpzB9SX471MGvKy5iHzcr63gObLmMaanGJWeWcdOITQOQrEjZQSG8Onrx0ztJdRp4nSoEUv04kZMBxIgYd7apmZLQYUp7SdJ7cSQmvtTwo6LBk5kfFf6IcHk2rZvuXPXT0aVmuowtTbHnmcsVN5efY2jh6I3HcZComE8pgnrmOIGe4cLw",
 });
+
+const builder = ImageUrlBuilder(client);
+
+export const urlFor = (source) => builder.image(source)
