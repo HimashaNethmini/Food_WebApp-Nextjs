9d8d8cc265438760f17cc64dfe7336c1933c1a0e
HimashaNethmini <himashanethmini05@gmail.com>
2023-10-04 19:12:39 +0530

    map the data

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,6 +1,29 @@
-export default function Menu()  {
-    return (
-      
-    )
-  
+import Image from "next/image";
+import css from "../styles/Menu.module.css";
+
+export default function Menu({ pizzas }) {
+  console.log(pizzas);
+  return (
+    <div className={css.container}>
+      <div className={css.heading}>
+        <span>OUR MENU</span>
+        <span>Menu That Always</span>
+        <span>Make You Fall in Love</span>
+      </div>
+
+      {/* mapping the array */}
+      {pizzas.map((pizza,id)=> {
+        return(
+          <div className={css.pizza} key={id}>
+
+            <div className={css.ImageWrapper}>
+              <Image src={src} alt="" />
+            </div>
+
+          </div>
+        )
+      })}
+
+    </div>
+  );
 }
