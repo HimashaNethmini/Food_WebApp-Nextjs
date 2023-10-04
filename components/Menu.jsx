41bedfa9c2999b595e95808a7a094b146673fa42
HimashaNethmini <himashanethmini05@gmail.com>
2023-10-04 19:24:22 +0530

    render sanity images to client

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,8 +1,9 @@
 import Image from "next/image";
 import css from "../styles/Menu.module.css";
+import { urlFor } from "../library/client";
 
 export default function Menu({ pizzas }) {
-  console.log(pizzas);
+  
   return (
     <div className={css.container}>
       <div className={css.heading}>
@@ -13,11 +14,15 @@ export default function Menu({ pizzas }) {
 
       {/* mapping the array */}
       {pizzas.map((pizza,id)=> {
+
+        const src = urlFor(pizza.image).url()
         return(
           <div className={css.pizza} key={id}>
 
             <div className={css.ImageWrapper}>
-              <Image src={src} alt="" />
+              <Image loader= {()=> src} src={src} alt="" 
+              objectFit="cover"
+              layout="fill" />
             </div>
 
           </div>
